9656effac7a34cfe09c3adf150ceb30e784202a1
drpiotr <79456861+drpiotr@users.noreply.github.com>
2021-02-25 21:17:21 +0100

    dzien3
    dzien3

---
+++ dzien3/zad4_2502.java
@@ -0,0 +1,58 @@
+public class zad4_2502 {
+        public static void main(String[] args) {
+                int i=0;
+                for(i = i+1; i < 10; i++) {
+                        System.out.print(" " + i);
+                }
+                System.out.println(" ");
+                i = 0;
+                for(i = i+1; i < 19; i++) {
+                        i = i+1;
+                        System.out.print(" " + i);
+                }
+                System.out.println(" ");
+                i = 0;
+                for(i = i+1; i < 28; i++) {
+                        i = i+2;
+                        System.out.print(" " + i);
+                }
+                System.out.println(" ");
+                i = 0;
+                for(i = i+1; i < 37; i++) {
+                        i = i+3;
+                        System.out.print(" " + i);
+                }
+                System.out.println(" ");
+                i = 0;
+                for(i = i+1; i < 46; i++) {
+                        i = i+4;
+                        System.out.print(" " + i);
+                }
+                System.out.println(" ");
+                i = 0;
+                for(i = i+1; i < 55; i++) {
+                        i = i+5;
+                        System.out.print(" " + i);
+                }
+                System.out.println(" ");
+                i = 0;
+                for(i = i+1; i < 64; i++) {
+                        i = i+6;
+                        System.out.print(" " + i);
+                }
+                System.out.println(" ");
+                i = 0;
+                for(i = i+1; i < 73; i++) {
+                        i = i+7;
+                        System.out.print(" " + i);
+                }
+                System.out.println(" ");
+                i = 0;
+                for(i = i+1; i < 82; i++) {
+                        i = i+8;
+                        System.out.print(" " + i);
+                }
+        }
+        }
+
+
---
+++ dzien3/zad5_2502.java
@@ -0,0 +1,16 @@
+public class zad5_2502 {
+    public static void main(String[] args) {
+        int wysokoscTrojkata = 5;
+
+
+        for (int i = 0; i < wysokoscTrojkata; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("X ");
+            }
+            System.out.println();
+        }
+    }
+}
+
