467479e070c96b2aeb68c2dd1f7efdbdf6def294
drpiotr <79456861+drpiotr@users.noreply.github.com>
2021-02-25 11:59:04 +0100

    1

--- dzien2/zad22302.java
+++ dzien2/zad22302.java
@@ -1,23 +1,15 @@
-package pl.am.liczby.fibbonaci;
 public class zad22302 {
-    public static long fibonacci(long n) {
-        if (n==1 || n==2) {
-            return 1;
-        }
-
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
-
     public static void main(String[] args) {
-        System.out.println(fibonacci(1));
-        System.out.println(fibonacci(2));
-        System.out.println(fibonacci(3));
-        System.out.println(fibonacci(4));
-        System.out.println(fibonacci(5));
-        System.out.println(fibonacci(6));
-        System.out.println(fibonacci(7));
-
-
+        long x1 = 1;
+        long x2 = 1;
+        for (int i = 3; i <= 7; i++) {
+            long temp = x1 + x2;
+            x1 = x2;
+            x2 = temp;
+        }
+        System.out.println(x2);
     }
 }
+}
+
 
